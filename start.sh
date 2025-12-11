19117cf0201552a95ed7130b6d903147eedcb9e3
Dashboard <juanariasceballos01@gmail.com>
2025-12-11 10:53:16 -0500

    trabajo

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ echo "Module: backend.app.main"
 echo ""
 
 # Iniciar uvicorn
-python -m uvicorn backend.app.main:app --host 0.0.0.0 --port $PORT --workers 1
+python -m uvicorn backend.main:app --host 0.0.0.0 --port $PORT --workers 1 --proxy-headers
